2212bfd3644d707491157504ba71cc4a621995f8
muntherabdlrahman <munther.abdlrahman@gmail.com>
2021-04-27 00:19:09 +0300

    new project is finished

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Aboutme-lab02
+### Welcome to my personal website, if you my frind or you are stranger , I hope that we can be best friends.
+### You can know more by this website about me.
+
+
+
+##### Big Thanks to Instrctor:Baraa'h and  TA: Abdelaziz to help me to create this website
+#####  taken 2.30 hour to create it
---
+++ js/app.js
@@ -0,0 +1,115 @@
+'use strict'
+
+alert('You are on Munther Abdlrahman  personal page, Welcome! ')
+
+let firstAns=prompt('Do you know me?')
+
+switch (firstAns.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        console.log('Welcome to  my page, my friend..!')
+        document.write('Welcome to  my page, my friend..!')
+        document.write('You are Welcome' )
+        break;
+      
+      case 'no':
+      case 'n':
+        console.log('Nice to see you , all honor to know you!')
+        document.write('Nice to see you , all honor to know you!')
+        document.write('Happy opportunity to get to know you!')
+        break;
+        default:
+            console.log('welcome');
+            document.write('welcome :)')
+
+
+
+}
+
+let secAns=prompt('Do you want to know where I live?')
+switch (secAns.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        console.log('I Hope that you can visit me soon *_* ')
+        document.write('I Hope that you can visit me soon *_*                                                                                                                                                                              ')
+        break;
+    case 'NO':
+    case 'N':
+    console.log('smoedays i will host you to my home with my pleasure :)')
+    document.write('Jordan-Zarqa; smoedays i will host you to my home with my pleasure :)                                                                                                                                                  ')
+    break;
+
+    default :
+    console.log('See you soon')
+
+
+
+}
+
+
+let thirdANS=prompt('Do you know where Zarqa is ??')
+switch(thirdANS.toLowerCase()){
+    case 'yes':
+    case 'y':
+        console.log('perfect')
+        document.write('Yup it is the same city that my uinversity is                                                                                                                                                                     ')
+        break;
+    case 'no':
+    case 'n':
+        console.log(' Really :(  you should know it!')
+        document.write('Zarqa (Arabic: الزرقاء‎) is the capital of Zarqa Governorate in Jordan. Its name means "the blue (city)". It had a population of 635,160 inhabitants in 2015, and is the most populous city in Jordan after Amman.')
+                 
+        default:
+            console.log('welcome :( ')
+   
+    }
+
+
+let fourAns=prompt('Did you know that I am an electrical engineer?')
+switch(fourAns.toLowerCase()){
+    case 'yes':
+    case 'y':
+        console.log('dont be surprised ! ')
+        document.write('Graduated from Hashemite University in Aug,2018 and my major field is Power Electrical                                                                                                                                             ')
+        break;
+        case 'no':
+        case 'n':
+        console.log('OK, should I intreduce myself :)')
+        document.write('I am an electrical engineer graduated from Hashemite University in Aug,2018 and my major field is Power electrical, My studies was general electrical engineering so i studied power, electronics; and telecommunications. ' )
+        break;
+        default:
+            console.log('dont wanna knOW me?')
+
+        
+}
+
+let fiveAns=prompt('Guess what, did you like Shawerma??');
+switch(fiveAns.toLowerCase()){
+    case 'yes':
+    case 'y':
+        console.log('That is your chance to eat Shawerma from my hand hhhh :)')
+        document.write('I worked as a Shawerma maker and i have a 2 years experiance with that work you should try my Shawerma ! so Delecious!                                                                                                               ')
+
+        break;
+        case 'no':
+        case 'n':
+            console.log('Hm; so weird  ')
+            document.write('You should try it from my hand!')
+            break;
+            default:
+                console.log('Just try it')
+
+
+}
+
+
+
+
+
+
+
+let sixAns=confirm('you will be my friend');
+console.log('Best friend!');
+document.write('Best friend!');
+
+
